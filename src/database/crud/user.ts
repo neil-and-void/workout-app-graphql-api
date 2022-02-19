287b025c0517ffc8f4bed55c872772bc122c18fe
neilZon <neilcviloria@gmail.com>
2022-02-18 20:03:33 -0700

    create and read user functions

---
+++ src/database/crud/user.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+/**
+ * CREATE new user in db
+ *
+ * @param email email of new user
+ * @param firstname firstname of new user
+ */
+export const createUser = async (email: string, firstname: string) => {
+  return await prisma.users.create({
+    data: {
+      email: 'elsa@prisma.io',
+      firstname: 'Elsa Prisma',
+    },
+  });
+};
+
+/**
+ * READ user by id
+ *
+ * @param id id of the user to get
+ */
+export const getUser = async (id: number) => {
+  return await prisma.users.findUnique({
+    where: {
+      id: id,
+    },
+  });
+};
