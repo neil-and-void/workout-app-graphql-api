6a1a9f61b58178978069917047dbb201b77df691
neilZon <neilcviloria@gmail.com>
2022-02-25 00:15:06 -0700

    authentication in user query

--- src/graphql/queries/user.ts
+++ src/graphql/queries/user.ts
@@ -1,3 +1,4 @@
+import { AuthenticationError } from 'apollo-server-express';
 import crud from '../../database/crud';
 
 interface UserArgs {
@@ -5,7 +6,8 @@ interface UserArgs {
 }
 
 export const userQueries = {
-  user: async (_: any, { id }: UserArgs) => {
+  user: async (_: any, { id }: UserArgs, context: any) => {
+    if (!context.user) throw new AuthenticationError('Unauthenticated');
     return await crud.user.getUser(id);
   },
 };
