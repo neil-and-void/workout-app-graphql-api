251cbbc29378a6b4b05195313ded8379c7b6f4c3
neilZon <neilcviloria@gmail.com>
2022-05-08 12:24:07 -0600

    add set resolver

--- src/graphql/mutations/set.ts
+++ src/graphql/mutations/set.ts
@@ -2,8 +2,9 @@ import { AuthenticationError } from 'apollo-server-express';
 import crud from '../../database/crud';
 
 export const setMutations = {
-  createSet: async (_: any, { set }: any, context: any) => {
+  createSet: async (_: any, args: any, context: any) => {
     if (!context.user) throw new AuthenticationError('Unauthenticated');
+    const { set } = args;
     return await crud.set.createSet(set);
   },
 };
